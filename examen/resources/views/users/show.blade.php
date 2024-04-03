1003bcf4d5aa215b26a694642f6a83e94caedebb
amberopsteen <amber-opsteen@hotmail.com>
2024-04-03 15:08:23 +0200

    Detail page for user
    Made a detail page where you can find the details for a specific user.

--- examen/resources/views/users/show.blade.php
+++ examen/resources/views/users/show.blade.php
@@ -1,28 +1,20 @@
 <x-app>
     <div class="p-8 bg-blue-100 h-screen">
         <div class="flex items-end justify-between my-5">
-            <h1 class="font-bold text-3xl">Users</h1>
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <x-buttons.primary-button type="submit">Log out</x-buttons.primary-button>
-            </form>
+            <h1 class="font-bold text-3xl">User details</h1>
+            <x-buttons.primary-link :href="route('users.index')">Back to overview</x-buttons.primary-link>
         </div>
         <table class="border-collapse w-full">
             <tr class="text-left">
                 <x-table.th>Name</x-table.th>
                 <x-table.th>Email</x-table.th>
                 <x-table.th>Role</x-table.th>
-                <x-table.th>Show</x-table.th>
             </tr>
-            @foreach($users as $user)
                 <tr>
                     <x-table.td>{{ $user->name }}</x-table.td>
                     <x-table.td>{{ $user->email }}</x-table.td>
                     <x-table.td>{{ $user->role }}</x-table.td>
-                    <x-table.td><x-buttons.primary-link :href="route('users.show', $user->id)">Show</x-buttons.primary-link>
-                    </x-table.td>
                 </tr>
-            @endforeach
         </table>
     </div>
 </x-app>
