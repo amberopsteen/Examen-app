37e7bd36a00e7ac9dced92bc26d4124b279434c6
amberopsteen <amber-opsteen@hotmail.com>
2024-04-03 12:00:43 +0200

    Migrations
    Migrations made based on ERD.

--- examen/database/migrations/2024_04_03_095240_create_tasks_table.php
+++ examen/database/migrations/2024_04_03_095240_create_tasks_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['user', 'admin'])->default('user');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('start_datetime');
+            $table->dateTime('end_datetime');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
